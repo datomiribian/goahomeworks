f72b1e7485aaf4f31b85159eedfe9cfee104442a
ggofrex <datomiribi@gmail.com>
2024-05-31 23:11:22 +0400

    first commit

---
+++ day32/classwork.py
@@ -0,0 +1,53 @@
+# goa = "goa the best"
+
+# new_string = ""
+
+# for i in range(12): 
+
+# finals_str =  ""
+# for i in range(10):
+#     print(i)
+
+
+
+# result = ""
+# for i in range(1):
+#     result += "g"
+#     print(result)
+  
+# result = ""    
+
+# for i in range(1):
+
+#     result += "o"
+#     print(result)
+
+
+
+#     result = ""
+
+#     for i in range (1):
+#         result += "a"
+#         print(result)
+
+# es teqsti dagveprinta speisebis gareshe
+# print("goa is the best")
+# goa = "goa is the best"
+# new_goa = ""
+# for char in range(len(goa)):
+#     if goa [char] != "":
+#       new_goa += goa[char]
+
+
+# for  char in goa : 
+#     print(i)
+#     print(new_goa ) 
+
+
+# print(goa)
+# print(goa[0])
+# for char in range():
+# for i in goa :
+#        if i != " ":
+#              new_goa += i 
+#              print(new_goa)
---
+++ day34/classwork.py
@@ -0,0 +1,11 @@
+goachadpeople = ["batoni nika", "batoni gabrieli" ,"batoni, daviti", "gio", 'indauri', "fexi", 'pasanauruli, xinkali',"zirapi", "lomfxvi", "tengiza", "jemala " "jimbreiko", "merabiko", "gvelgevsla", "drakoni", "xvliki", "siraqlema"]
+
+goa_string = "goa"
+
+
+#start, end, step. 18
+
+
+print(goachadpeople[:4])
+
+
