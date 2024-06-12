05128dff9936d02c0b8334eeb2f5d6916edb642b
datomiribian <datomiribi@gmail.com>
2024-06-12 14:06:15 +0400

    commit

---
+++ day38/classwork/classwork.py
@@ -0,0 +1,31 @@
+# anime = ["white beard", "zoro" "naruto" "baki" "maki"]
+# print(anime)
+# anime.append("kakashi hatake")
+# anime.append("yorichi")
+# print(anime)
+
+
+# damateba / add  / append
+# Name =  ["jumala" , "axera" , "genada", "tengiza" , "nugzara" ]
+# Name.append("zangi") 
+# print(Name)
+
+numbers = []
+even_numbers = []
+for i in range (10):
+    num = input("enter your number: ")
+    numbers.append(num)
+    
+    numbers[0]
+    numbers[1] 
+    numbers[2]
+    numbers[3]
+    numbers[4]
+    numbers[5]
+    numbers[6]
+    numbers[7]
+    numbers[8]
+    numbers[9]
+
+    for i in range(len(numbers)):
+        print(i)
---
+++ day38/homework/homework.py
@@ -0,0 +1,21 @@
+FILMS = ["честь дракона" , " ip man" , "ფორსაჟი" , "ფორსაჟ1" , "ფორსაჟი2" ,"ფორსაჟი3","ფორსაჟი4" ,"ფორსაჟი5","ფორსაჟი6","ფორსაჟი7"]
+print(FILMS)
+print(FILMS[0])
+print(FILMS[1])
+print(FILMS[2])
+print(FILMS[3])
+print(FILMS[4])
+print(FILMS[5])
+print(FILMS[6])
+print(FILMS[7])
+print(FILMS[8])
+print(FILMS[9])
+
+
+
+friends = ["gio" "tengo" "leqso" "ilia" "vato" "luka" "nika" "nini" "mari" "nini"]
+print(friends)
+
+
+friends2 = ["andria alavadze", "aleko tirkia", , "anastasia khvedelidze" , "andria lakobishvili" ,"andria korchilava" , "andria samsonidze" , "andria shavana" , "barbare tinikashvili" , "beridze mate" , "dachi sasandrishvili" , "data tezelashvili" , "dato berkacashvili" , "dato miribiani" , "davit grdzelishvili" , "davit janashia" , "deme biganashvili" , "erekle karagozishvili" , "gabriel gviniashvili" , "batoni gabriel molodini" , "gio alada" ,"gio goginidze" , "giorgi vanishvili" , "giviko kavrishvili"] 
+
