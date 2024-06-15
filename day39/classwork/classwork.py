c4380fbfedd9be11fbcbae6cecb6f78394f4436a
datomiribian <datomiribi@gmail.com>
2024-06-15 22:34:52 +0400

    commit

---
+++ day39/classwork/classwork.py
@@ -0,0 +1,16 @@
+# print("gamarjoba aircie rac ginda chveni konsultanti dagexmarebat")
+# prise = input("ra fasis nivti gindat")
+# discount = input("ra fasdaklebit gindat nivti")
+# print(int(prise) * int(discount) / 100 )
+
+# number = []
+# input_number = int(input("wish number for 0 to 100 :"))
+# for i in range(10):
+#   if input_number % 2 ==  0:
+#     even.append(input_number)  
+
+
+# else:
+#  odd.append(input_number)
+# print(odd)
+# print(even)
