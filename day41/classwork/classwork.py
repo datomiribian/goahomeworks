af100b24bca77dff777ba6758a36c0fa0a05c27a
datomiribian <datomiribi@gmail.com>
2024-06-23 12:49:38 +0400

    commit

---
+++ day41/classwork/classwork.py
@@ -0,0 +1,20 @@
+# numbers = [1,2,3,43,3,345,5,6776,65,876,434,253,54,545,645643,7645,654,]
+
+# counter = 0
+
+# for i in range(len(numbers)):
+#  if len(str(numbers[i])) == 1:
+#   counter += 1 
+# print(counter)
+
+
+# numbers = [1,2,3,4,5,6,7,8,9,10]
+# upsite__down_list = [] 
+# for i in range (len(numbers)):
+#     upsite__down_list.append(numbers[-i-1])
+#     print(upsite__down_list)
+
+# numbers = [1,5,40,15,13,100,76,30]
+# for i in range(len(numbers)):
+#     if numbers[i] % 5 == 0 :
+#         print(numbers[i])
